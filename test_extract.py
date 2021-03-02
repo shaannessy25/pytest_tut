68843c76049d88b06ce00f6f9d2540c6a13436a6
shaannessy25 <shaan.hurley25@gmail.com>
2021-03-01 20:45:38 -0800

    finished all challenges

---
+++ test_extract.py
@@ -0,0 +1,30 @@
+# by Kami Bigdely
+# Replace nested conditional with gaurd clauses
+
+def extract_position(line):
+    if not line:
+        pos = None
+    else:
+        if 'debug' in line or 'error' in line:
+            pos = None
+        else:
+            if 'x:' in line:
+                start_index = line.find('x:') + 2
+                pos = line[start_index:] # from start_index to the end.
+            else: 
+                pos = None
+    return pos
+
+if __name__ == "__main__":
+    result1 = extract_position('|error| numerical calculations could not converge.')
+    print(result1)
+    result2 = extract_position('|update| the positron location in the particle accelerator is x:21.432')
+    print(result2)
+
+
+
+
+def test_extract_position():
+    assert extract_position("") == None
+    assert extract_position("error numerical calculations could not converge") == None
+    assert extract_position("the positron location in the particle accelerator is x:21.432") == '21.432'
